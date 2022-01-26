29fb45e5fb1fde7ebd4de9e1cb59b84136f684e9
Ankit Kumar Sinha <85090273+Ankit-sinha07@users.noreply.github.com>
2022-01-26 12:04:00 +0530

    This is a responsive web page project

---
+++ Project-1 responsive Website/JavaScript/resp.js
@@ -0,0 +1,12 @@
+burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+navList = document.querySelector('.nav-list')
+rightNav = document.querySelector('.rightnav')
+
+
+
+burger.addEventListener('click',()=>{
+    rightNav.classList.toggle('v-class');
+    navList.classList.toggle('v-class');
+    navbar.classList.toggle('h-nav');
+})
